5953739bb6c0f65fafee60c894fd4ca39c39c3d4
Grégory NEUT <gregory.neut@gmail.com>
2022-03-04 16:01:56 +0100

    add icon to fire wallet

--- components/HotWallet/HotWallet.tsx
+++ components/HotWallet/HotWallet.tsx
@@ -1,12 +1,16 @@
 import useHotWallet from '@hooks/useHotWallet'
 import HotWalletAccount from './HotWalletAccount'
+import { FireIcon } from '@heroicons/react/outline'
 
 const HotWallet = () => {
   const { tokenAccountsInfo } = useHotWallet()
 
   return (
     <div className="bg-bkg-2 p-4 md:p-6 rounded-lg transition-all">
-      <h3 className="bg-bkg-2 mb-4 flex items-center">Hot Wallet</h3>
+      <h3 className="bg-bkg-2 mb-4 flex items-center">
+        <FireIcon className="flex-shrink-0 h-5 mr-1 text-primary-light w-5" />
+        Hot Wallet
+      </h3>
 
       <div style={{ maxHeight: '350px' }} className="overflow-y-auto space-y-3">
         {tokenAccountsInfo?.map((tokenAccountInfo) => (
