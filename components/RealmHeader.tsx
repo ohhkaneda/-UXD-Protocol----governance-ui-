d632b3da105d5f87f2e67f7b71af10289a2f45da
Grégory NEUT <gregory.neut@gmail.com>
2022-04-20 12:36:34 +0200

    remove Params from realm header

--- components/RealmHeader.tsx
+++ components/RealmHeader.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import useRealm from 'hooks/useRealm';
-import { CogIcon, GlobeAltIcon } from '@heroicons/react/outline';
-import Link from 'next/link';
+import { GlobeAltIcon } from '@heroicons/react/outline';
 import { TwitterIcon } from './icons';
-import useQueryContext from 'hooks/useQueryContext';
 
 const RealmHeader = () => {
-  const { fmtUrlWithCluster } = useQueryContext();
-  const { realmInfo, realmDisplayName, symbol } = useRealm();
+  const { realmInfo, realmDisplayName } = useRealm();
 
   return (
     <div className="pb-4">
@@ -32,12 +29,6 @@ const RealmHeader = () => {
           <div className="animate-pulse bg-bkg-3 h-10 w-40 rounded-md" />
         )}
         <div className="flex items-center space-x-4">
-          <Link href={fmtUrlWithCluster(`/dao/${symbol}/params`)}>
-            <a className="flex items-center cursor-pointer text-primary-light hover:text-primary-dark text-sm">
-              <CogIcon className="flex-shrink-0 h-4 mr-1 w-4" />
-              Params
-            </a>
-          </Link>
           {realmInfo?.website ? (
             <a
               className="default-transition flex items-center text-primary-light hover:text-primary-dark text-sm"
