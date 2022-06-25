23b1f5b625d4b3fc3e683f3287b3b05d2946aaa4
Grégory NEUT <gregory.neut@gmail.com>
2022-06-25 15:25:07 +0400

    Implement Saber Swap

--- utils/uiTypes/proposalCreationTypes.ts
+++ utils/uiTypes/proposalCreationTypes.ts
@@ -247,6 +247,13 @@ export interface SaberPoolsWithdrawOneForm {
   uiMinimumTokenAmount?: number;
 }
 
+export interface SaberPoolsSwapForm {
+  governedAccount?: GovernedMultiTypeAccount;
+  poolName?: SupportedSaberPoolNames;
+  uiAmountIn?: number;
+  uiMinimumAmountOut?: number;
+}
+
 export interface SaberPeripheryRedeemAllTokensFromMintProxyForm {
   governedAccount?: GovernedMultiTypeAccount;
   mintName?: QuarryMineSupportedMintName;
@@ -489,6 +496,7 @@ export enum InstructionEnum {
   RaydiumRemoveLiquidity,
   SaberPoolsDeposit,
   SaberPoolsWithdrawOne,
+  SaberPoolsSwap,
   SaberPeripheryRedeemAllTokensFromMintProxy,
   SolendCreateObligationAccount,
   SolendInitObligationAccount,
